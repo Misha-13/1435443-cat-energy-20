a1819f98ace3622a26c624e24e3e5fcb8d3aeb04
Misha-13 <m.a.cherkasov@yandex.ru>
2020-09-06 13:47:54 +0300

    Дополняет адаптивную графику 4

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,9 +1,6 @@
 var navigationPane = document.querySelector('.main-nav');
 var navigationButton = document.querySelector('.main-nav__toggle');
 
-if (navigationPane.classList.contains ('no-js-nav')) {
-  navigationPane.classList.remove('no-js-nav');
-}
 
 if (window.innerWidth<768) {
   navigationPane.classList.add('main-nav--closed');
@@ -22,29 +19,15 @@ navigationButton.addEventListener('click', function(){
 });
 
 window.addEventListener('resize', function(){
-  /* if (window.innerWidth<768) {
-    navigationPane.classList.add('main-nav--closed');
-  }
-
-  if (navigationPane.classList.contains('main-nav--closed')) {
-    navigationPane.classList.remove('main-nav--closed');
-    navigationButton.classList.remove('main-nav__toggle--close');
-  }; */
-
-  if (navigationPane.classList.contains('main-nav--closed')) {
-    navigationPane.classList.remove('main-nav--closed');
-    navigationButton.classList.remove('main-nav__toggle--open');
-  /* } else {
-    navigationButton.classList.remove('main-nav__toggle--close'); */
-  };
-
-  if (window.innerWidth<768) {
-    if(!navigationPane.classList.contains('main-nav--closed')){
-      navigationPane.classList.add('main-nav--closed');
-      navigationButton.classList.add('main-nav__toggle--close');
-    } else {
+  if (window.innerWidth>768) {
+    if (navigationPane.classList.contains('main-nav--closed')) {
+      navigationPane.classList.remove('main-nav--closed');
+      navigationButton.classList.remove('main-nav__toggle--close');
       navigationButton.classList.add('main-nav__toggle--open');
-    }
+    } else {
+      navigationButton.classList.remove('main-nav__toggle--open');
+      navigationButton.classList.add('main-nav__toggle--close');
+    };
   };
 });
 
